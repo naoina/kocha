b8e7a7e826f5521bc4db59bc519dcac801b813ef
Naoya Inada <naoina@kuune.org>
2013-11-05 07:50:25 +0900

    Add HTTPS detection methods

--- request.go
+++ request.go
@@ -2,6 +2,8 @@ package kocha
 
 import (
 	"net/http"
+	"os"
+	"strings"
 )
 
 type Request struct {
@@ -12,4 +14,20 @@ func NewRequest(req *http.Request) *Request {
 	return &Request{
 		Request: req,
 	}
+}
+
+func (r *Request) Scheme() string {
+	switch {
+	case os.Getenv("HTTPS") == "on", os.Getenv("HTTP_X_FORWARDED_SSL") == "on":
+		return "https"
+	case os.Getenv("HTTP_X_FORWARDED_SCHEME") != "":
+		return os.Getenv("HTTP_X_FORWARDED_SCHEME")
+	case os.Getenv("HTTP_X_FORWARDED_PROTO") != "":
+		return strings.Split(os.Getenv("HTTP_X_FORWARDED_PROTO"), ",")[0]
+	}
+	return "http"
+}
+
+func (r *Request) IsSSL() bool {
+	return r.Scheme() == "https"
 }
--- request_test.go
+++ request_test.go
@@ -2,6 +2,7 @@ package kocha
 
 import (
 	"net/http"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -15,6 +16,73 @@ func TestNewRequest(t *testing.T) {
 	expected := &Request{
 		Request: req,
 	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expect %v, but %v", expected, actual)
+	}
+}
+
+func TestRequestScheme(t *testing.T) {
+	req := &Request{}
+	func() {
+		os.Setenv("HTTPS", "on")
+		defer os.Clearenv()
+		actual := req.Scheme()
+		expected := "https"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expect %v, but %v", expected, actual)
+		}
+	}()
+
+	func() {
+		os.Setenv("HTTP_X_FORWARDED_SSL", "on")
+		defer os.Clearenv()
+		actual := req.Scheme()
+		expected := "https"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expect %v, but %v", expected, actual)
+		}
+	}()
+
+	func() {
+		os.Setenv("HTTP_X_FORWARDED_SCHEME", "file")
+		defer os.Clearenv()
+		actual := req.Scheme()
+		expected := "file"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expect %v, but %v", expected, actual)
+		}
+	}()
+
+	func() {
+		os.Setenv("HTTP_X_FORWARDED_PROTO", "gopher")
+		defer os.Clearenv()
+		actual := req.Scheme()
+		expected := "gopher"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expect %v, but %v", expected, actual)
+		}
+
+		os.Setenv("HTTP_X_FORWARDED_PROTO", "https, http, file")
+		actual = req.Scheme()
+		expected = "https"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expect %v, but %v", expected, actual)
+		}
+	}()
+}
+
+func TestRequestIsSSL(t *testing.T) {
+	req := &Request{}
+	actual := req.IsSSL()
+	expected := false
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expect %v, but %v", expected, actual)
+	}
+
+	os.Setenv("HTTPS", "on")
+	defer os.Clearenv()
+	actual = req.IsSSL()
+	expected = true
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expect %v, but %v", expected, actual)
 	}
