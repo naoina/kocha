9c3c787090d9d00ac0075b50d0de64378e07a25d
Naoya Inada <naoina@kuune.org>
2014-02-28 01:05:27 +0900

    Add 'GetenvDefault'

--- util_test.go
+++ util_test.go
@@ -3,6 +3,7 @@ package kocha
 import (
 	"go/format"
 	"html/template"
+	"os"
 	"reflect"
 	"regexp"
 	"testing"
@@ -61,6 +62,26 @@ func Test_SplitExt(t *testing.T) {
 			t.Errorf("Expect %v, but %v", expected[1], actual[1])
 		}
 	}
+}
+
+func TestGetenvDefault(t *testing.T) {
+	func() {
+		actual := GetenvDefault("TEST_KOCHA_ENV", "default value")
+		expected := "default value"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("GetenvDefault(%q, %q) => %q, want %q", "TEST_KOCHA_ENV", "default value", actual, expected)
+		}
+	}()
+
+	func() {
+		os.Setenv("TEST_KOCHA_ENV", "set kocha env")
+		defer os.Clearenv()
+		actual := GetenvDefault("TEST_KOCHA_ENV", "default value")
+		expected := "set kocha env"
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("GetenvDefault(%q, %q) => %q, want %q", "TEST_KOCHA_ENV", "default value", actual, expected)
+		}
+	}()
 }
 
 func Test_normPath(t *testing.T) {
