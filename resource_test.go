31d9f9c2ca8e7f39d563718823df78fec0bb84f2
Naoya Inada <naoina@kuune.org>
2014-07-07 15:14:33 +0900

    resource: Add tests

--- resource_test.go
+++ resource_test.go
@@ -1,11 +1,44 @@
-package kocha
+package kocha_test
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/naoina/kocha"
+)
 
 func TestResourceSet_Add(t *testing.T) {
-	t.Error("pending")
+	rs := kocha.ResourceSet{}
+	for _, v := range []struct {
+		name string
+		data interface{}
+	}{
+		{"text1", "test1"},
+		{"text2", "test2"},
+	} {
+		rs.Add(v.name, v.data)
+		actual := rs[v.name]
+		expected := v.data
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf(`ResourceSet.Add("%#v", %#v) => %#v; want %#v`, v.name, v.data, actual, expected)
+		}
+	}
 }
 
 func TestResourceSet_Get(t *testing.T) {
-	t.Error("pending")
+	rs := kocha.ResourceSet{}
+	for _, v := range []struct {
+		name string
+		data interface{}
+	}{
+		{"text1", "test1"},
+		{"text2", "test2"},
+	} {
+		rs[v.name] = v.data
+		actual := rs.Get(v.name)
+		expected := v.data
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf(`ResourceSet.Get("%#v") => %#v; want %#v`, v.name, actual, expected)
+		}
+	}
 }
