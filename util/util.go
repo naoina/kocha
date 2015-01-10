756694d81a1910a685b9bdfa0b17a7ba8116c04c
Naoya Inada <naoina@kuune.org>
2015-01-10 12:47:48 +0900

    util: ToCamelCase and ToSnakeCase now use go-stringutil implementation

--- util/util.go
+++ util/util.go
@@ -28,6 +28,7 @@ import (
 
 	"github.com/daviddengcn/go-colortext"
 	"github.com/jessevdk/go-flags"
+	"github.com/naoina/go-stringutil"
 )
 
 const (
@@ -42,46 +43,12 @@ var (
 	ImportDir = build.ImportDir
 )
 
-// ToCamelCase returns a copy of the string s with all Unicode letters mapped to their camel case.
-// It will convert to upper case previous letter of '_' and first letter, and remove letter of '_'.
 func ToCamelCase(s string) string {
-	if s == "" {
-		return ""
-	}
-	result := make([]rune, 0, len(s))
-	upper := false
-	for _, r := range s {
-		if r == '_' {
-			upper = true
-			continue
-		}
-		if upper {
-			result = append(result, unicode.ToUpper(r))
-			upper = false
-			continue
-		}
-		result = append(result, r)
-	}
-	result[0] = unicode.ToUpper(result[0])
-	return string(result)
+	return stringutil.ToUpperCamelCase(s)
 }
 
-// ToSnakeCase returns a copy of the string s with all Unicode letters mapped to their snake case.
-// It will insert letter of '_' at position of previous letter of uppercase and all
-// letters convert to lower case.
 func ToSnakeCase(s string) string {
-	if s == "" {
-		return ""
-	}
-	var result bytes.Buffer
-	result.WriteRune(unicode.ToLower(rune(s[0])))
-	for _, c := range s[1:] {
-		if unicode.IsUpper(c) {
-			result.WriteRune('_')
-		}
-		result.WriteRune(unicode.ToLower(c))
-	}
-	return result.String()
+	return stringutil.ToSnakeCase(s)
 }
 
 // SplitExt returns pair of file name and extension.
--- util/util_test.go
+++ util/util_test.go
@@ -43,38 +43,6 @@ func (m orderedOutputMap) GoString() string {
 	return fmt.Sprintf("map[string]interface{}{%v}", strings.Join(keys, ", "))
 }
 
-func TestToCamelCase(t *testing.T) {
-	for v, expected := range map[string]string{
-		"":        "",
-		"kocha":   "Kocha",
-		"KochA":   "KochA",
-		"koch_a":  "KochA",
-		"k_oc_ha": "KOcHa",
-		"k_Oc_hA": "KOcHA",
-	} {
-		actual := ToCamelCase(v)
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("%v: Expect %v, but %v", v, expected, actual)
-		}
-	}
-}
-
-func TestToSnakeCase(t *testing.T) {
-	for v, expected := range map[string]string{
-		"":       "",
-		"kocha":  "kocha",
-		"Kocha":  "kocha",
-		"kochA":  "koch_a",
-		"kOcHa":  "k_oc_ha",
-		"ko_cha": "ko_cha",
-	} {
-		actual := ToSnakeCase(v)
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("%v: Expect %v, but %v", v, expected, actual)
-		}
-	}
-}
-
 func Test_SplitExt(t *testing.T) {
 	for value, expected := range map[string][]string{
 		"/":               {"/", ""},
