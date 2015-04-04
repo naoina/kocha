19935e71a8ac214a2b2f59d98003d7b1e2591555
Naoya Inada <naoina@kuune.org>
2015-04-04 23:33:14 +0900

    bench: Add simple benchmark

---
+++ kocha_bench_test.go
@@ -0,0 +1,42 @@
+package kocha_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/naoina/kocha"
+)
+
+type nullResponseWriter struct {
+	header http.Header
+}
+
+func newNullResponseWriter() *nullResponseWriter {
+	return &nullResponseWriter{
+		header: make(http.Header),
+	}
+}
+
+func (w *nullResponseWriter) Header() http.Header {
+	return w.header
+}
+
+func (w *nullResponseWriter) Write(b []byte) (int, error) {
+	return len(b), nil
+}
+
+func (w *nullResponseWriter) WriteHeader(n int) {
+}
+
+func BenchmarkServeHTTP(b *testing.B) {
+	app := kocha.NewTestApp()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			b.Fatal(err)
+		}
+		w := newNullResponseWriter()
+		app.ServeHTTP(w, req)
+	}
+}
